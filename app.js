b5db7edd5acffb6cf74eb34f60f9c8af9c072e16
j0ttaape <joaopedrotrajano59@gmail.com>
2025-11-05 09:23:39 -0300

    criando primeiro endpoint

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+import express from 'express'
+
+/*criando primeiro endpoint*/
 
 
-console.log("teste")
+//rodando o express
+const servidor = express();
+
+
+//criando endpoint
+
+servidor.get('/helloword', (req, resp) => {
+    //codigo do endpoint
+    resp.send('helloword mané')
+})
+
+
+
+
+
+//vinculando a api com uma porta
+servidor.listen(5001, () => console.log("API SUBIU NA PORTA 5001"));
