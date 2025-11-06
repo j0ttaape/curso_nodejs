fb1c59c4176c821811e3ad24b97919e139c0c6a0
j0ttaape <joaopedrotrajano59@gmail.com>
2025-11-05 21:22:57 -0300

    parametros com rota

--- app.js
+++ app.js
@@ -14,7 +14,51 @@ servidor.get('/helloword', (req, resp) => {
     resp.send('helloword mané')
 })
 
+servidor.get('/mensagem/boasvindas', (req, resp) => {
+    //codigo do endpoint
+    resp.send('olá sejam bem vindos')
+})
 
+servidor.get('/v2/mensagem/boasvindas', (req, resp) =>{
+    resp.send('Que bom que você está aqui!')
+})
+
+servidor.get('/mensagem/ocupado', (req, resp) => {
+    resp.send('Estou ocupado no momento')
+})
+
+servidor.get('/mensagem/ocupado/recado', (req,resp) => {
+    resp.send('Estou ocupado, deixe uma mensagem...')
+})
+
+
+servidor.get('/calculadora/:n1/:n2', (req,resp) =>{
+    let n1 = Number(req.params.n1);
+    let n2 = Number(req.params.n2);
+    let soma = n1 + n2;
+    resp.send(`A soma de ${n1} com ${n2} é igual a: ${soma} `)
+})
+
+servidor.get('/calculadoraSubtrair/:n1/:n2', (req,resp) => {
+    let n1 = Number(req.params.n1);
+    let n2 = Number(req.params.n2);
+    let sub = n1 - n2;
+    resp.send(`A subtração de ${n1} e ${n2} é igual a ${sub}`)
+})
+
+servidor.get('/calculadoraMultiplicar/:n1/:n2', (req,resp) =>{
+    let n1 = Number(req.params.n1);
+    let n2 = Number(req.params.n2);
+    let multiplicar = n1 * n2;
+    resp.send(`A multiplicação de ${n1} com ${n2} é igual a ${multiplicar}`)
+})
+
+servidor.get('/calculadoraDividir/:n1/:n2', (req,resp) => {
+    let n1 = Number(req.params.n1);
+    let n2 = Number(req.params.n2);
+    let divisao = n1 / n2;
+    resp.send(`A divisão de ${n1} com ${n2} é igual a ${divisao}`)
+})
 
 
 
